6994b3344dee2e7583b112b30eaa57f6f930c395
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-01-13 22:56:18 +0530

    Router implemented

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
+import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-import Home from './components/Home';
-import RenderComponent from './components/RenderComponent';
 import Services from './components/Services';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <About />
-      <Services />
-      <Contact />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/services' element={<Services />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
