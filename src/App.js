59814685a2ca851ef0b0871c3b00bbced4cb2e15
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-01-14 19:58:30 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Services from './components/Services';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/services' element={<Services />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
