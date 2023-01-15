d96c29541a6e05b9611acaff028b49dfa8c8595e
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-01-15 23:43:03 +0530

    navbar styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,43 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Navbar = () => {
     return (
-        <nav>
+        <MyNav>
             <div className='icon'>
                 <ul className='list'>
                     <li>
-                        <NavLink to='/'>Home</NavLink>
+                        <NavLink className='navbarlink' to='/'>Home</NavLink>
                     </li>
                     <li>
-                        <NavLink to='/about'>About</NavLink>
+                        <NavLink className='navbarlink' to='/about'>About</NavLink>
                     </li>
                     <li>
-                        <NavLink to='/services'>Services</NavLink>
+                        <NavLink className='navbarlink' to='/services'>Services</NavLink>
                     </li>
                     <li>
-                        <NavLink to='/contact'>Contact</NavLink>
+                        <NavLink className='navbarlink' to='/contact'>Contact</NavLink>
                     </li>
                 </ul>
             </div>
-        </nav>
+        </MyNav>
     );
 }
+
+const MyNav = styled.nav`
+.list{
+    display: flex;
+    gap: 4rem;
+    list-style: none;
+    .navbarlink{
+        &:hover,
+        &:active {
+            color: ${(theme) => theme.colors.helper};
+        }
+    }
+}
+
+`;
 
 export default Navbar;
