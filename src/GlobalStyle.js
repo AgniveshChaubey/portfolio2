0474e4f2f6914710cc180c93fd508915112aba07
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-01-15 23:29:14 +0530

    header designed

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -12,8 +12,39 @@ html{
 }
 
 h1{
+    color: ${({ theme }) => theme.colors.heading};
     font-size: 6rem;
     font-weight: 900;
+}
+
+h1{
+    color: ${({ theme }) => theme.colors.heading};
+    font-size: 4.4rem;
+    font-weight: 300;
+    white-space: normal;
+    text-align: center;
+}
+
+h3{
+    font-size: 1.8rem;
+    font-weight: 400;
+}
+
+p{
+    color: ${({ theme }) => theme.colors.text};
+    opacity: .7;
+    font-size: 1.65rem;
+    line-height: 1.5;
+    margin-top: 1rem;
+    font-weight: 400;
+}
+
+a{
+    text-decoration: none;
+}
+
+li{
+    list-style: none;
 }
 
 `;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 import Navbar from './Navbar';
 
 const Header = () => {
   return (
-    <header>
+    <MyHeader>
         <NavLink to='/'>
             <img src="../images/code-in-veins.png" alt="logo" />
         </NavLink>
         <Navbar />
-    </header>
+    </MyHeader>
   )
 }
+
+const MyHeader = styled.header`
+padding: 0 4.8rem;
+height: 10rem;
+background-color: ${({theme}) => theme.colors.bg};
+display: flex;
+justify-content: space-between;
+align-items: center;
+
+`;
 
 export default Header;
